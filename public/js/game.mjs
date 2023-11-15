879f32ede65302caf4f476f94aa1036eebe6e355
Eli <eli.serra173@gmail.com>
2023-11-15 05:45:51 +0100

    Add Client game script

---
+++ public/js/game.mjs
@@ -0,0 +1,11 @@
+import Player from './Player.mjs';
+import Collectible from './Collectible.mjs';
+import {generateStartPos, SizeHelper} from './gameHelper.mjs';
+import Ut from './utils.mjs';
+import controls from './controls.mjs';
+
+const startGame = ()=>{
+
+}
+
+window.addEventListener("load", startGame());
