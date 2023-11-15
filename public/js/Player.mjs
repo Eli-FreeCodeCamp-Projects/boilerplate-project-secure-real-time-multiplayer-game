8f568539ac2ba51b7b60e6d97522e5931de20d8d
Eli <eli.serra173@gmail.com>
2023-11-15 05:35:21 +0100

    Add player class constructor

--- public/js/Player.mjs
+++ public/js/Player.mjs
@@ -1,5 +1,16 @@
 import {generateStartPos, SizeHelper} from './gameHelper.mjs';
 class Player {
+  constructor({id, x, y, name=undefined, score=0, speed=5, main=false, size={}}) {
+    this.id = id;
+    this.x = x;
+    this.y = y;
+    this.name = name;
+    this.speed = speed;
+    this.score = score;
+    this.movementDirection = {};
+    this.isMain = main;
+    this.sh = new SizeHelper(size)
+  } 
 }
 
 export default Player;
